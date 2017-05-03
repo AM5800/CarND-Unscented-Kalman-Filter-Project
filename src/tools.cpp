3817fab44e8384a111bb40257878f271170650b9
am5800 <am5800@yandex.ru>
2017-05-03 19:57:27 +0200

    Implement CalculateRMSE and make it static

--- src/tools.cpp
+++ src/tools.cpp
@@ -5,14 +5,18 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
-
-Tools::~Tools() {}
-
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0)
+    throw std::exception("Estimation and Ground Truth size mismatch");
+
+  VectorXd sum = VectorXd::Zero(estimations.front().rows());
+
+  for (size_t i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = ground_truth[i] - estimations[i];
+    VectorXd residual_squared = residual.array() * residual.array();
+    sum += residual_squared;
+  }
+
+  return (sum / estimations.size()).array().sqrt();
 }
